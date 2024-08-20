f5e56f4456849526bc4a52c5d9b82cd5b643c71b
Ashik Ahammed Hridoy <35147425+ahammedhridoy@users.noreply.github.com>
2024-08-20 23:26:57 +0600

    Ui modification

--- frontend/src/Dashboard/DashboardLayout.jsx
+++ frontend/src/Dashboard/DashboardLayout.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import SideBar from './SideBar'
-import { Outlet } from 'react-router-dom'
+import SideBar from "./SideBar";
+import { Outlet } from "react-router-dom";
 
 export const DashboardLayout = () => {
   return (
-    <div className='flex gap-4 flex-col md:flex-row'>
+    <div className="flex flex-col gap-4 md:flex-row">
       <div>
-      <SideBar/>
+        <SideBar />
       </div>
-      <Outlet/>
+      <Outlet />
       {/* <div className='w-full h-screen overflow-y-hidden'>
-        <div className='h-screen bg-blue-500 w-full' id='user'></div>
+        <div className='w-full h-screen bg-blue-500' id='user'></div>
         <div className='h-screen bg-slate-300' id='inbox'>inbox</div>
       </div> */}
     </div>
-  )
-}
+  );
+};
